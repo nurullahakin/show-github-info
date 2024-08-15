321f37c3a65f903ea5491354527206bd99713d29
Oktay Aydoğan <31257020+oktayaydogan@users.noreply.github.com>
2024-08-15 22:40:16 +0300

    Upload repo

---
+++ src/extension.ts
@@ -0,0 +1,50 @@
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
+
+let myStatusBarItem: vscode.StatusBarItem;
+
+export function activate(context: vscode.ExtensionContext) {
+	myStatusBarItem = vscode.window.createStatusBarItem(
+		vscode.StatusBarAlignment.Left,
+		999999999
+	);
+
+	const repoInfo = getRepoAndAccountName();
+	if (repoInfo) {
+		myStatusBarItem.text = `$(repo) ${repoInfo}`;
+		myStatusBarItem.show();
+	}
+}
+
+function getRepoAndAccountName(): string | null {
+	try {
+		const workspaceFolders = vscode.workspace.workspaceFolders;
+		if (!workspaceFolders) {
+			return null;
+		}
+
+		const workspacePath = workspaceFolders[0].uri.fsPath;
+		const gitConfigPath = path.join(workspacePath, ".git", "config");
+
+		if (!fs.existsSync(gitConfigPath)) {
+			return null;
+		}
+
+		// .git/config dosyasını oku
+		const configContent = fs.readFileSync(gitConfigPath, "utf-8");
+
+		// URL'yi bul
+		const remoteUrlMatch = configContent.match(
+			/url = https:\/\/github\.com\/(.+)\.git/
+		);
+		if (remoteUrlMatch) {
+			return remoteUrlMatch[1]; // oktayaydogan/management-service gibi bir string döndürür
+		} else {
+			return null;
+		}
+	} catch (error) {
+		console.error("Error getting Git repo and account name:", error);
+		return null;
+	}
+}
