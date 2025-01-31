7c2fd79ee711f1578f66e798c9e87156ee0f2871
Nurullah Akın <nurullah.at.work@outlook.com>
2025-01-31 09:56:12 +0300

    format: tab to space

--- src/extension.ts
+++ src/extension.ts
@@ -5,47 +5,44 @@ import * as path from "path";
 let myStatusBarItem: vscode.StatusBarItem;
 
 export function activate(context: vscode.ExtensionContext) {
-	myStatusBarItem = vscode.window.createStatusBarItem(
-		vscode.StatusBarAlignment.Left,
-		999999999
-	);
+    myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 999999999);
 
-	const repoInfo = getRepoAndAccountName();
-	if (repoInfo) {
-		myStatusBarItem.text = `$(repo) ${repoInfo}`;
-		myStatusBarItem.show();
-	}
+    const repoInfo = getRepoAndAccountName();
+    if (repoInfo) {
+        myStatusBarItem.text = `$(repo) ${repoInfo}`;
+        myStatusBarItem.show();
+    }
 }
 
 function getRepoAndAccountName(): string | null {
-	try {
-		const workspaceFolders = vscode.workspace.workspaceFolders;
-		if (!workspaceFolders) {
-			return null;
-		}
+    try {
+        const workspaceFolders = vscode.workspace.workspaceFolders;
+        if (!workspaceFolders) {
+            return null;
+        }
 
-		const workspacePath = workspaceFolders[0].uri.fsPath;
-		const gitConfigPath = path.join(workspacePath, ".git", "config");
+        const workspacePath = workspaceFolders[0].uri.fsPath;
+        const gitConfigPath = path.join(workspacePath, ".git", "config");
 
-		if (!fs.existsSync(gitConfigPath)) {
-			return null;
-		}
+        if (!fs.existsSync(gitConfigPath)) {
+            return null;
+        }
 
-		const configContent = fs.readFileSync(gitConfigPath, "utf-8");
+        const configContent = fs.readFileSync(gitConfigPath, "utf-8");
 
-		let match = configContent.match(/url = https:\/\/github\.com\/(.+)\.git/);
-		if (match) {
-			return match[1];
-		}
+        let match = configContent.match(/url = https:\/\/github\.com\/(.+)\.git/);
+        if (match) {
+            return match[1];
+        }
 
-		match = configContent.match(/url = git@github\.com:(.+)\.git/);
-		if (match) {
-			return match[1];
-		}
+        match = configContent.match(/url = git@github\.com:(.+)\.git/);
+        if (match) {
+            return match[1];
+        }
 
-		return null;
-	} catch (error) {
-		console.error("Error getting Git repo and account name:", error);
-		return null;
-	}
+        return null;
+    } catch (error) {
+        console.error("Error getting Git repo and account name:", error);
+        return null;
+    }
 }
